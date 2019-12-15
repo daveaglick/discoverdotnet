959c0dd9856cb6182b78af35dc3d0f708a035c88
Dave Glick <dave@daveaglick.com>
2019-12-14 19:34:06 -0500

    Switching to production Netlify site

--- src/DiscoverDotnet/Pipelines/DeploySite.cs
+++ src/DiscoverDotnet/Pipelines/DeploySite.cs
@@ -18,7 +18,7 @@ namespace DiscoverDotnet.Pipelines
             OutputModules = new ModuleList
             {
                 new ThrowException(Config.FromSettings(x => x.ContainsKey("NETLIFY_TOKEN") ? null : "The setting NETLIFY_TOKEN must be defined")),
-                new DeployNetlifySite("f3900c88-7859-4b37-a7fc-628bb0d168fa", Config.FromSetting<string>("NETLIFY_TOKEN"))
+                new DeployNetlifySite("1b1625b7-f7ad-46a4-bd5f-0e02e8b0f37c", Config.FromSetting<string>("NETLIFY_TOKEN"))
             };
         }
     }
