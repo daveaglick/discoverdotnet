7005536c322203ddbd3bce6ff06af9cbb7621451
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2021-08-01 08:34:22 +0200

    Remove unused usings

--- Analyzers/ValidateProjectTags.cs
+++ Analyzers/ValidateProjectTags.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DiscoverDotnet.Pipelines.Projects;
-using Microsoft.Extensions.Logging;
 using Statiq.Common;
 
 namespace DiscoverDotnet.Analyzers
